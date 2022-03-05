4af81c1418dc9ddd6d5951b0230642e0794e1430
Mark <mark@murphymark.me>
2022-03-05 10:47:28 -0500

    loop over projectIds

--- includes.mjs
+++ includes.mjs
@@ -26,15 +26,15 @@ async function printSlugs(toc) {
 }
 
 async function getToc(projectId, branchId) {
-      function manageErrors(response) {
-        if (!response.ok) {
-          if (response.status == 404) {
-            throw Error(response.statusText);
-          }
-          return; // will print '200 - ok'
-        }
-        return response;
+  function manageErrors(response) {
+    if (!response.ok) {
+      if (response.status == 404) {
+        throw Error(response.statusText);
       }
+      return; // will print '200 - ok'
+    }
+    return response;
+  }
 
   return fetch(
     `https://stoplight.io/api/v1/projects/${projectId}/table-of-contents?branch=${branchId}`
@@ -46,72 +46,29 @@ async function getToc(projectId, branchId) {
 }
 
 async function checkTocSlugs(projectId, branchId) {
+  for (let i = 0; i < projectId.length; i++) {
+    const element = projectId[i];
 
+    const idArray = await getToc(element, branchId);
+    const masterArray = await getToc(element, 'master');
+    let errors = idArray.filter(
+      (slug) =>
+        // let matchPattern = slug.match(/\d+/g);
+        masterArray.includes(slug) !== true
+      // matchPattern != null ? getStatus(slug) : null;
+    );
 
-
-  // const asyncFunc = async () => {
-  //     //   const unresolvedPromises = arr.map(n => calc(n));
-  //     //   const unresolvedPromises = idArray.map(n => getStatus(n));
-  //     const unresolvedPromises = idArray.map((n) => console.log(n));
-  //     const results = await Promise.all(unresolvedPromises);
-  //   };
-
-  //   asyncFunc();
-
-  //   function manageErrors(response) {
-  //     if (!response.ok) {
-  //       if (response.status == 404) {
-  //         throw Error(response.statusText);
-  //       }
-  //       return; // will print '200 - ok'
-  //     }
-  //     return response;
-  //   }
-    //   function manageErrors(response) {
-    //     if (!response.ok) {
-    //       if (response.status == 404) {
-    //         throw Error(response.statusText);
-    //       }
-    //       return; // will print '200 - ok'
-    //     }
-    //     return response;
-    //   }
-
-    //   let errors = [];
-
-    //   function getStatus(slug) {
-    //    fetch(
-    //       `https://stoplight.io/api/v1/projects/${projectId}/nodes/${slug}?branch=master`
-    //     )
-    //       .then(manageErrors)
-    //       .then((response) => response.json())
-    //       .then((result) => null)
-    //       .catch((error) => errors.push(slug));
-    //   }
-
-    //   function getStatus(slug) {
-    //     return fetch(
-    //       `https://stoplight.io/api/v1/projects/${projectId}/nodes/${slug}?branch=${branchId}`
-    //     )
-    //       .then(manageErrors)
-    //       .then((response) => response.json())
-    //       .then((result) => null)
-    //       .catch((error) => console.log(slug));
-    //   }
-  const idArray = await getToc(projectId, branchId);
-  // const idArray = ['dove', 'cat', 'hat', 'beer']
-  const masterArray = await getToc(projectId, 'master');
-  // const masterArray = ['dove','bat','cat','beer'];
-
-  let errors = idArray.filter(slug => 
-    // let matchPattern = slug.match(/\d+/g);
-    masterArray.includes(slug) !== true
-    // matchPattern != null ? getStatus(slug) : null;
-  );
-
-
-  console.log(errors)
-  return errors;
+    console.log(
+      '🚀 ~ file: includes.mjs ~ line 72 ~ checkTocSlugs ~ errors',
+      errors
+    );
+    return errors;
+  }
 }
 
-checkTocSlugs('cHJqOjIwNjAz', 'filter-test-branch');
+let apiRef = 'cHJqOjIwNjAz';
+let devDocs = 'cHJqOjI4MDIz';
+
+let projectIdArr = [apiRef, devDocs];
+
+checkTocSlugs(projectIdArr, 'filter-test-branch');
